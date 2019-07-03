3857029505d1a174db13ef6590761d887353c379
DalexKraus <dalex.kraus@gmail.com>
2019-07-03 12:25:58 +0200

    Documented Timer

--- src/at/dalex/grape/graphics/Timer.java
+++ src/at/dalex/grape/graphics/Timer.java
@@ -2,8 +2,16 @@ package at.dalex.grape.graphics;
 
 import static org.lwjgl.glfw.GLFW.glfwGetTime;
 
+/**
+ * This class is used to time the Main Game Loop.
+ * It can also be used to determine FPS or UPS.
+ * (Frames per second, updates per second)
+ *
+ * The Delta Time is also calculated here.
+ */
 public class Timer {
 
+	/* Some sick private fields */
 	private double lastLoopTime;
 	private float deltaTime;
 	private float timeCount;
@@ -12,26 +20,47 @@ public class Timer {
 	private int ups;
 	private int upsCount;
 
+	/**
+	 * Initializes the timer.
+	 * Basically just sets the last loop time to
+	 * the current time.
+	 */
 	public void init() {
 		lastLoopTime = getTime();
 	}
 
+	/**
+	 * @return The time the game is running
+	 */
 	public double getTime() {
 		return glfwGetTime();
 	}
 
+	/**
+	 * @return The time it took for the last frame to (update and) render.
+	 */
 	public float getDelta() {
 		return this.deltaTime;
 	}
 
+	/**
+	 * Increments the amount of frames
+	 */
 	public void updateFPS() {
 		fpsCount++;
 	}
 
+	/**
+	 * Increments the amount of updates
+	 */
 	public void updateUPS() {
 		upsCount++;
 	}
 
+	/**
+	 * Update the Timer, this will result in recalculation of the FPS & UPS
+	 * and delta time
+	 */
 	public void update() {
 		double time = getTime();
 		deltaTime = (float) (time - lastLoopTime);
@@ -49,14 +78,23 @@ public class Timer {
 		}
 	}
 
+	/**
+	 * @return The amount of frames per second
+	 */
 	public int getFPS() {
 		return fps > 0 ? fps : fpsCount;
 	}
-	
+
+	/**
+	 * @return The amount of updates per second
+	 */
 	public int getUPS() {
 		return ups > 0 ? ups : upsCount;
 	}
-	
+
+	/**
+	 * @return The time of the last loop iteration
+	 */
 	public double getLastLoopTime() {
 		return lastLoopTime;
 	}
