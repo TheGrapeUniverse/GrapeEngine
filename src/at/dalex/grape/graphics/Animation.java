a8d01e19e8fda53b348117b6c9f40c33d21330c0
DalexKraus <dalex.kraus@gmail.com>
2019-07-03 14:25:03 +0200

    Documented Animation

--- src/at/dalex/grape/graphics/Animation.java
+++ src/at/dalex/grape/graphics/Animation.java
@@ -10,6 +10,8 @@ import at.dalex.grape.graphics.graphicsutil.ImageUtils;
  * Calling <code>getImage()</code> will give you the appropriate
  * image in the animation cycle.
  *
+ * //TODO: Update rendering system to only use a single texture internally. (Not hundreds)
+ *
  * @author dalex
  */
 public class Animation {
@@ -19,39 +21,82 @@ public class Animation {
     private int numFrames;              //Count of total frames
     private int framesPerSecond = 2;    //Amount of frames per second.
 
+    /**
+     * Private constructor to avoid instantiation
+     */
+    private Animation() { }
+
+    /**
+     * Sets the images of the animation
+     * @param frames The images of the animation
+     */
     public void setFrames(Image[] frames) {
         this.frames = frames;
         numFrames = frames.length;
     }
 
+    /**
+     * Updates the animation respecting the delta time for a consistent animation cycle.
+     * @param delta The time it took the last frame to render.
+     */
     public void update(double delta) {
         currentFrame += (framesPerSecond * delta);
         if ((int) currentFrame >= numFrames) currentFrame = 0;
     }
+
+    /**
+     * @return The amount of frames per second
+     */
     public int getFramesPerSecond() {
         return this.framesPerSecond;
     }
 
+    /**
+     * Sets the amount of frames per second
+     * @param framesPerSecond The amount of frames the animation should have per second.
+     */
     public void setFramesPerSecond(int framesPerSecond) {
         this.framesPerSecond = framesPerSecond;
     }
 
+    /**
+     * @return The index of the current frame
+     */
     public int getCurrentFrame() {
         return (int) currentFrame;
     }
 
+    /**
+     * Sets a new frame as current in the animation cycle
+     * @param index Index of the frame in the atlas
+     */
     public void setCurrentFrame(int index) {
         currentFrame = index;
     }
 
+    /**
+     * @return The number of frames this animation has
+     */
     public int getFrameCount() {
         return frames.length;
     }
 
+    /**
+     * @return The Image of the current frame in the animation cycle
+     */
     public Image getImage() {
         return frames[(int) currentFrame];
     }
 
+    /**
+     * Loads a new Animation from disk using an animation atlas.
+     *
+     * @param animationAtlas The image where every frame is stored
+     * @param frameWidth The width of one frame
+     * @param frameHeight The height of one frame
+     * @param framesPerSecond The amount of frames the animation should have per second.
+     * @return The created {@link Animation}
+     */
     public static Animation loadAnimation(BufferedImage animationAtlas, int frameWidth, int frameHeight, int framesPerSecond) {
         Animation animation = new Animation();
         Image[] frames = new Image[animationAtlas.getWidth() / frameWidth];
